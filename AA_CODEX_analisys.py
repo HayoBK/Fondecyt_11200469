74124d1c7cee11cec53e706490c46f7c456f9169
HayoBK <hbreinbauer@uchile.cl>
2023-03-29 21:26:14 -0300

    NeuroPsi con Drops para perfilar por edad

--- AA_CODEX_analisys.py
+++ AA_CODEX_analisys.py
@@ -9,9 +9,16 @@ Py_Processing_Dir=home+"/OneDrive/2-Casper/00-CurrentResearch/001-FONDECYT_11200
 file = Py_Processing_Dir+'AA_CODEX.xlsx'
 # Si tu poner tu archivo .csv en el mismo directorio que tu archivo .py, no hay que hacer tanto jaleo y puede decir:
 # file = 'AC_PupilLabs_SyncData_Faundez.csv'
-
 df = pd.read_excel(file, index_col=0) #Aqui cargamos tu base de datos en la variable "df"
+df = df[['Grupo','Edad']].copy()
+Banish_List =['P13','P06','P18','P19','P20']
+df = df[~df.index.isin(Banish_List)]
+df = df.dropna()
+df = df[df.index.notnull()]
+
 print('Cargada la base de datos')
+print (df.sort_values(['Grupo', 'Edad'], ascending=[True, True]))
 print(df.groupby(['Grupo']).mean())
+print(df.groupby(['Grupo']).size())
 ax= sns.boxplot(data=df, x= 'Grupo', y='Edad')   # Aqui construimos el grafico.
 plt.show()
